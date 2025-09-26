634b5aaeed131589083d7a726e18f05d6583f32e
Julia Scheaffer <me@gisch.dev>
2025-09-26 11:26:16 -0500

    Cleaned up sorting network code

--- RecMath/SortingNetwork.lean
+++ RecMath/SortingNetwork.lean
@@ -12,31 +12,16 @@ variable {α : Type u} [LinearOrder α] {n : Nat}
 
 def Tuple (α n) := Fin n -> α
 
--- instance Tuple.instGetElem {α n} : GetElem (Tuple α n) Nat α (fun _ i ↦ i < n) where
---  getElem t i h := t (Fin.mk i h)
+-- Not using these anymore, but i can change my definitoins to use them if wanted
+-- def Sorted (t : Tuple α n) : Prop := Monotone t
+-- def SortingFunction (f : Tuple α n -> Tuple α n) : Prop := ∀ (t : Tuple α n), Sorted (f t)
 
--- #synth GetElem (Tuple Int 2) Nat Int _
--- #eval ((GetElem.getElem (![1, 2] : Tuple Int 2) 0 (by sorry)) : Nat)
-
--- def Tuple.toVector (t : Tuple α n) : Vector α n := (Vector.range n).map (fun i ↦ t (Fin.mk i (by sorry)))
-
--- def Tuple.nil : Tuple α 0 := by nofun
-
--- instance Tuple.instInhabitedTuple0 : Inhabited (Tuple α 0) where
---   default := Tuple.nil
-
-def Sorted (t : Tuple α n) : Prop := Monotone t
-
-def SortingFunction (f : Tuple α n -> Tuple α n) : Prop := ∀ (t : Tuple α n), Sorted (f t)
-
-
-
--- def IndexPair (n : Nat) := {p : Fin n × Fin n // p.fst < p.snd}
 structure IndexPair (n : Nat) where
   i : Fin n
   j : Fin n
   h : i < j
 
+-- These proofs of the cardinality of IndexPair n probably arent useful, but they were fun
 instance : IsEmpty (IndexPair 0) where
   false a := IsEmpty.false a.i
 
@@ -57,25 +42,6 @@ def IndexPair.permute (p : IndexPair n) (t : Tuple α n) (k : Fin n) : Fin n :=
   else
     k
 
-def IndexPair.apply (p : IndexPair n) (t : Tuple α n) : Tuple α n := t ∘ p.permute t
-
--- I dont know what to call this lemma, it needs a better name
-theorem IndexPair.apply.asdf (p : IndexPair n) (t : Tuple α n)
-    : p.apply t p.i ≤ p.apply t p.j := by
-  obtain ⟨i, j, hp⟩ := p
-  simp only [apply, Function.comp, permute, ↓reduceIte]
-  grind only [le_of_not_ge]
-
-theorem IndexPair.apply.monotoneOn_ij (p : IndexPair n) (t : Tuple α n) :
-    MonotoneOn (p.apply t) {p.i, p.j} := by
-  intro a ha b hb a_le_b
-  by_cases a_eq_b : a = b
-  { subst a_eq_b; rfl }
-  have ⟨i_eq_a, j_eq_b⟩: p.i = a ∧ p.j = b := by
-    grind only [= Set.mem_singleton_iff, = Set.mem_insert_iff, IndexPair]
-  subst i_eq_a j_eq_b
-  exact asdf p t
-
 theorem IndexPair.permute.involutive {p : IndexPair n} {t : Tuple α n}
     : Function.Involutive (p.permute t) := by
   grind only [Function.Involutive, permute]
@@ -89,6 +55,26 @@ def IndexPair.toPerm (t : Tuple α n) (p : IndexPair n) : Equiv.Perm (Fin n) whe
   right_inv k := by
     change (p.permute t ∘ p.permute t) k = k
     rw [Function.Involutive.comp_self IndexPair.permute.involutive, id]
+
+def IndexPair.apply (p : IndexPair n) (t : Tuple α n) : Tuple α n := t ∘ p.permute t
+
+-- I dont know what to call this lemma, it needs a better name
+theorem IndexPair.apply.asdf (p : IndexPair n) (t : Tuple α n)
+    : p.apply t p.i ≤ p.apply t p.j := by
+  obtain ⟨i, j, hp⟩ := p
+  simp [apply, Function.comp, permute, ↓reduceIte]
+  grind only [le_of_not_ge]
+
+theorem IndexPair.apply.monotoneOn_ij (p : IndexPair n) (t : Tuple α n) :
+    MonotoneOn (p.apply t) {p.i, p.j} := by
+  intro a ha b hb a_le_b
+  by_cases a_eq_b : a = b
+  { subst a_eq_b; rfl }
+  have ⟨i_eq_a, j_eq_b⟩: p.i = a ∧ p.j = b := by
+    grind only [= Set.mem_singleton_iff, = Set.mem_insert_iff, IndexPair]
+  subst i_eq_a j_eq_b
+  exact asdf p t
+
 
 def ComparisonNetwork (n : Nat) := List (IndexPair n)
 
@@ -115,54 +101,9 @@ theorem ComparisonNetwork.trivial_network_sorts : trivial_network.Sorts := by
 #eval ComparisonNetwork.trivial_network.apply ![3, 2]
 #eval ComparisonNetwork.trivial_network.apply ![1, 2]
 
-
-
 -- [x] Sorting networks can be lists of index pair
 -- [x] Need some sort of proposition that says a network sorts
--- Forms a setoid with an equivalence for reordering independent pairs
+-- [ ] Forms a setoid with an equivalence for reordering independent pairs
 
--- then i can write a function with a given tuple forms a permutation that is sorted
--- which can be applied to the tuple to give a sorting function
-
-def Prod.compare_and_exchange (p : α × α) : α × α :=
-  let (a, b) := p
-  if a ≤ b then (a, b) else (b, a)
-
-@[grind =]
-theorem Prod.compare_and_exchange.eq_min_max {p : α × α}
-    : p.compare_and_exchange = (min p.fst p.snd, max p.fst p.snd) := by
-  rw [Prod.compare_and_exchange]
-  split_ifs <;> ext <;> order
-
-example {p : α × α} : p.compare_and_exchange.fst ≤ p.compare_and_exchange.snd := by
-  rw [Prod.compare_and_exchange]
-  split_ifs <;> order
-
-def Tuple.compare_and_exchange (i j : Fin n) (_ : i < j) (t : Tuple α n) : Tuple α n :=
-  fun k ↦
-    if k = i ∨ k = j then
-      let (less, greater) := (t i, t j).compare_and_exchange
-      if k = i then less else greater
-    else
-      t k
-
-def Tuple.compare_and_exchange.MonotoneOn_ij {i j : Fin n} {h : i < j} (t : Tuple α n)
-    : MonotoneOn (compare_and_exchange i j h t) {i, j} := by
-  intro a ha b hb a_le_b
-  by_cases a_eq_b : a = b
-  { subst_vars; rfl }
-  have : i = a ∧ j = b := by grind only [= Set.mem_singleton_iff, = Set.mem_insert_iff]
-  grind only [= compare_and_exchange, = Prod.compare_and_exchange.eq_min_max,
-    inf_le_iff, le_max_left]
-
--- First proof of a valid sorting function!
-example : SortingFunction (Tuple.compare_and_exchange (α := α) (n := 2) 0 1 (by decide)) := by
-  intro t
-  rw [Sorted, <- monotoneOn_univ, (by grind : Set.univ (α := Fin 2) = {0, 1})]
-  exact Tuple.compare_and_exchange.MonotoneOn_ij t
-
--- def Tuple.compare_and_exchange.Equiv (i j : Fin n) (f : Fin n → α) : Equiv.Perm (Fin n) where
---   toFun := sorry
---   invFun := sorry
---   left_inv := by sorry
---   right_inv := by sorry
+-- [x] i can write a function with a given tuple forms a permutation (ComparisonNetwork.toPerm)
+-- [x] which can be applied to the tuple `ComparisonNetwork.apply`
